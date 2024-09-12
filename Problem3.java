fb5a9e6dbc71ca1a0795d2be0095b20dcf292051
LHNT2k7 <ltri9573@gmail.com>
2024-09-12 10:15:39 -0700

    fgh

--- Problem3.java
+++ Problem3.java
@@ -2,13 +2,13 @@ public class Problem3
 {
   public static void main(String[] args)
   { 
-    double duacondaulong = 257.84793
+    double duacondaulong = 257.84793;
     System.out.println("The number is " + duacondaulong);
-    duacondaulong *= 1000
+    int duacondaulong = (int) (duacondaulong * 1000);
     System.out.println(duacondaulong % 10);
-    duacondaulong /= 10
+    duacondaulong /= 10;
     System.out.println(duacondaulong % 10);
-    duacondaulong /= 10
+    duacondaulong /= 10;
     System.out.println(duacondaulong % 10);
   }
 }
