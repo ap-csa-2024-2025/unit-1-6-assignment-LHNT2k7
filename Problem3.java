c592f328a20c00e53ea6f44d7f3e90d144df69f7
LHNT2k7 <ltri9573@gmail.com>
2024-09-12 10:13:09 -0700

    wert

--- Problem3.java
+++ Problem3.java
@@ -1,1 +1,14 @@
-
+public class Problem3
+{
+  public static void main(String[] args)
+  { 
+    double duacondaulong = 257.84793
+    System.out.println("The number is " + duacondaulong);
+    duacondaulong *= 1000
+    System.out.println(duacondaulong % 10);
+    duacondaulong /= 10
+    System.out.println(duacondaulong % 10);
+    duacondaulong /= 10
+    System.out.println(duacondaulong % 10);
+  }
+}
